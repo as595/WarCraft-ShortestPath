e593b13f062af051713b83d285ab6db2edaa6781
as595 <anna.scaife@manchester.ac.uk>
2024-02-16 12:28:17 +0000

    Add files via upload

--- models.py
+++ models.py
@@ -151,7 +151,7 @@ class Combinatorial(pl.LightningModule):
 		# pass the predicted weights through the dijkstra algorithm:
 		suggested_paths = self.solver(weights, self.lambda_val, self.neighbourhood_fn) # only positional arguments allowed (no keywords)
         
-        true_paths = z_test.view(z_test.size()[0], -1)
+		true_paths = z_test.view(z_test.size()[0], -1)
 
 		accuracy = exact_match_accuracy(true_paths, suggested_paths)
 		self.log('exact match accuracy [test]', accuracy)
