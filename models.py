da2a3f4f26fb6e9f1dee56e75b81d4aa9d213bb9
as595 <anna.scaife@manchester.ac.uk>
2024-02-16 12:37:37 +0000

    Add files via upload

--- models.py
+++ models.py
@@ -152,9 +152,9 @@ class Combinatorial(pl.LightningModule):
 		suggested_paths = self.solver(weights, self.lambda_val, self.neighbourhood_fn) # only positional arguments allowed (no keywords)
         
 		true_paths = z_test.view(z_test.size()[0], -1)
-		print(true_paths.shape, suggested_paths.shape)
+		suggested_paths = suggested_paths.view(suggested_paths.size()[0], -1)
 		
-		accuracy = exact_match_accuracy(true_paths, suggested_paths)
+		accuracy = exact_match_accuracy(z_test, suggested_paths)
 		self.log('exact match accuracy [test]', accuracy)
 
 		true_weights = y_test.view(y_test.size()[0], -1)
